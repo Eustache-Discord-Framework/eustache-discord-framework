17fbbffcd0f364a89e5bad01f8de2f0eb769dcd8
gmvqbu <gmvqbu@users.noreply.github.com>
2021-03-01 20:12:35 +0100

    help.js
    Deux méthodes pour afficher :
    - toutes les commandes
    - une seule commande

--- src/commands/commands/util/help.js
+++ src/commands/commands/util/help.js
@@ -14,30 +14,58 @@ class HelpCommand extends Command {
                     key: 'cmd',
                     label: 'Commande',
                     type: 'command',
-                    default: ''
+                    default: 'none'
                 }
             ]
         });
     }
 
-    async run(msg, args) {
-        const commands = Array.from(this.client.registry.findCommand())
+    run(msg, args) {
+        const cmd = 'cmd' in args ? args.cmd : null;
         const embed = new MessageEmbed({
             color: 'RANDOM',
-            title: `Message d'aide`,
-            fields: [
-                {
-                    name: 'Commandes',
-                    value: commands.map(cmd => cmd.hidden ? '' : `\`${this.client.commandPrefix}${cmd.name}\` ${cmd.description ? `: ${cmd.description}` : ''}`).join("\n")
-                }
-            ],
             footer: {
                 text: this.client.user.username
             },
             timestamp: new Date()
+        });
+
+        if (cmd.name === 'none') msg.reply(this.all(embed))
+        else msg.reply(this.single(embed, cmd));
+    }
+
+    all(embed) {
+        let commands = Array.from(this.client.registry.findCommand());
+        commands = commands.filter(cmd => cmd.hidden === false);
+        commands = commands.map(cmd => {
+            let name = this.client.commandPrefix + cmd.name;
+            let description = cmd.description ? `: ${cmd.description}` : '';
+            return `\`${name}\`${description}`
         })
 
-        msg.reply(embed);
+        if (commands && commands.length > 0) {
+            return embed
+                .setTitle("Message d'aide")
+                .addField('Commands', commands.join("\n"));
+        } else {
+            return embed
+                .setTitle('Mince alors...')
+                .setDescription("Il n'y a aucune commande à afficher ici.");
+        }
+    }
+
+    single(embed, cmd) {
+        const name = this.client.commandPrefix + this.name;
+        const args = this.argsCollector ? ' ' + this.argsCollector.args.map(arg => arg.format).join(' ') : '';
+        const usage = name + args;
+        const description = cmd.description ? `: ${cmd.description}` : '';
+        const aliases = (cmd.aliases && cmd.aliases.length > 0) ? cmd.aliases.map(alias => `\`${this.client.commandPrefix + alias}\``).join(', ') : null;
+
+        embed.setTitle(`Message d'aide - Commande \`${name}\``);
+        embed.addField('Commande', `\`${usage}\`${description}`);
+        if (aliases) embed.addField('Alias', aliases);
+
+        return embed;
     }
 }
 
