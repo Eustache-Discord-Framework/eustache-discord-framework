c970439001bcafd177ff0142b39eaf1aa18b60ed
gmvqbu <gmvqbu@users.noreply.github.com>
2021-03-02 18:16:22 +0100

    refactoring index.js

--- src/index.js
+++ src/index.js
@@ -5,8 +5,18 @@ module.exports = {
     EustacheDispatcher: require('./dispatcher'),
     EustacheRegistry: require('./registry'),
     Command: require('./commands/base'),
-    Argument: require('./commands/Argument'),
     ArgumentCollector: require('./commands/ArgumentCollector'),
+    Argument: require('./commands/Argument'),
+    ArgumentType: require('./types/base'),
 
-    version: require('../package').version
+    version: require('../package').version,
+
+    // commands
+    HelpCommand: require('./commands/commands/util/help'),
+    PingCommand: require('./commands/commands/util/ping'),
+    UnknownCommand: require('./commands/commands/util/unknown'),
+
+    // types
+    StringArgumentType: require('./types/string'),
+    CommandArgumentType: require('./types/command'),
 }
